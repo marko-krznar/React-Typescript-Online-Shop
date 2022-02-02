f17403ed314683829fa5f1effafb5611e3a75601
Marko-batman <krznarm26@gmail.com>
2022-02-02 20:39:29 +0100

    Added homepage container

---
+++ src/containers/Homepage/Homepage.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Products from '../../components/Products';
+import data from "../../components/data";
+
+
+function Homepage() {
+
+    const { products } = data;
+
+    return (
+        <div>
+            <main className="p-4">
+                <h1 className="text-center display-1 font-weight-bold">
+                    BatM's
+                </h1>
+                <p className="text-center text--hd-desc">
+                    This is a test site made for learning. For using coupon
+                    write in field PROMO30. It will reduce total for 30%.
+                </p>
+                <Products products={products} />
+            </main>
+        </div>
+    );
+}
+
+export default Homepage;
