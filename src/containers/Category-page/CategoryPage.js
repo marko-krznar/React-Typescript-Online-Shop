804177e17243a420b4fc51573dbcce94978de7f0
Marko-batman <krznarm26@gmail.com>
2022-02-02 21:17:16 +0100

    Created container for Category page and separate product items to its own component

---
+++ src/containers/Category-page/CategoryPage.js
@@ -0,0 +1,15 @@
+import data from "../../components/data";
+import ProductItem from "../../components/Product-item/ProductItem";
+
+export default function CategoryPage() {
+
+    return (
+        <div className="d-flex flex-wrap align-items-start mt-5">
+            <div className="productList productList--col-4 pr-4">
+                {data.products.map((product, index) => (
+                    <ProductItem key={index} product={product} />
+                ))}
+            </div>
+        </div>
+    );
+}
