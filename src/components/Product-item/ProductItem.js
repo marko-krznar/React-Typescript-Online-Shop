aa7cbc59b646052136898048b8add753a60c0477
Marko-batman <krznarm26@gmail.com>
2022-02-04 21:47:17 +0100

    Style for CategoryPage

--- src/components/Product-item/ProductItem.js
+++ src/components/Product-item/ProductItem.js
@@ -1,27 +1,4 @@
-// import React, { useState } from "react";
-
 export default function ProductItem({ product }) {
-  //   const [cartItems, setcartItems] = useState([]);
-
-  // Add to Cart and increase if product is already in cart
-  //   const addToCartHandler = (product) => {
-  //     Checks if product is in the cart
-  //     const existProduct = cartItems.find((x) => x.id === product.id);
-  //     If exist change only qty
-  //     if (existProduct) {
-  //       setcartItems(
-  //         cartItems.map((x) =>
-  //           x.id === product.id
-  //             ? { ...existProduct, qty: existProduct.qty + 1 }
-  //             : x
-  //         )
-  //       );
-  //     } else {
-  //       Dodavanje proizvoda u cart
-  //       setcartItems([...cartItems, { ...product, qty: 1 }]);
-  //     }
-  //   };
-
   return (
     <div className="product__item">
       <div className="position-relative">
@@ -39,12 +16,7 @@ export default function ProductItem({ product }) {
             <span>{product.price} EUR</span>
           </div>
           <div className="block--add-to-cart">
-            <button
-              className="btn btn-primary mb-2"
-              //   onClick={() => addToCartHandler(product)}
-            >
-              Add to Cart
-            </button>
+            <button className="btn btn-primary mb-2">See more</button>
           </div>
         </div>
       </div>
