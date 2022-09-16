419ac70840a649c9b669e854ee26f0a21e37ca7a
marko <krznarm26@gmail.com>
2022-09-16 23:33:55 +0200

    Added functionality for cart page

--- src/components/Cart-item/CartItem.js
+++ src/components/Cart-item/CartItem.js
@@ -1,26 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { CartContext } from "../../api/CartContext";
 
 export default function CartItem({ cartItem }) {
-  return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <th>Product</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>All</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{cartItem.name}</td>
-            <td>{cartItem.price}</td>
-            <td>{cartItem.name}</td>
-            <td>{cartItem.name}</td>
-          </tr>
-        </tbody>
-      </table>
-    </>
-  );
+	const [cart, setCart] = useContext(CartContext);
+
+	const handleDeleteCartItem = () => {
+		const newCart = cart.filter((prod) => prod.id !== cartItem.id);
+		setCart(newCart);
+	};
+
+	return (
+		<tr>
+			<td>{cartItem.name}</td>
+			<td>{cartItem.price}</td>
+			<td>
+				<button onClick={handleDeleteCartItem}>x</button>
+			</td>
+		</tr>
+	);
 }
--- src/containers/Product-page/ProductPage.js
+++ src/containers/Product-page/ProductPage.js
@@ -1,10 +1,20 @@
+import React, { useContext } from "react";
+
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 
+import { CartContext } from "../../api/CartContext";
 import "./style.scss";
 
 export const ProductPage = () => {
+	const [cart, setCart] = useContext(CartContext);
 	const location = useLocation();
+
+	const handleAddToCart = () => {
+		const productExist = cart.find((prod) => prod.id === location.state.id);
+		if (productExist) return;
+		setCart([location.state, ...cart]);
+	};
 
 	return (
 		<div className="page product">
@@ -18,7 +28,9 @@ export const ProductPage = () => {
 				<h2>{location?.state?.name}</h2>
 				<p>{location?.state?.desc}</p>
 				<p>Price: {location?.state?.price} €</p>
-				<button className="btn btn--light">Add to cart</button>
+				<button className="btn btn--light" onClick={handleAddToCart}>
+					Add to cart
+				</button>
 			</div>
 		</div>
 	);
