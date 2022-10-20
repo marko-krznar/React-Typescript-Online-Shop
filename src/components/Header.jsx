f8603fd788753f6a0f7ae533dd26a920ee5f514c
marko <krznarm26@gmail.com>
2022-10-20 22:22:22 +0200

    Added responsive header style and animations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { Link } from "react-router-dom";
 import { GiBatMask, GiHamburgerMenu } from "react-icons/gi";
@@ -7,31 +7,48 @@ import { CartContext } from "../local/CartContext";
 
 const Header = () => {
 	const [cart] = useContext(CartContext);
+	const [isActive, setIsActive] = useState(false);
+
+	const handleMenu = () => {
+		setIsActive(!isActive);
+	};
+
+	const closeMenu = () => {
+		setIsActive(false);
+	};
 
 	return (
 		<nav className="menu d-flex justify-content-end align-items-center">
 			<div className="menu-logo-wrapper">
-				<Link className="menu-logo" to="/">
+				<Link className="menu-logo" to="/React-Webshop">
 					<GiBatMask />
 				</Link>
 			</div>
-			<ul className="menu-list d-flex">
+			<button className="menu-hamburger" onClick={handleMenu}>
+				<GiHamburgerMenu />
+			</button>
+			<ul
+				className={
+					isActive ? "menu-list d-flex active" : "menu-list d-flex"
+				}
+			>
 				<li className="menu-item">
-					<Link to="/">Home</Link>
+					<Link to="/React-Webshop" onClick={closeMenu}>
+						Home
+					</Link>
 				</li>
 				<li className="menu-item">
-					<Link to="/React-Webshop/products">Products</Link>
+					<Link to="/React-Webshop/products" onClick={closeMenu}>
+						Products
+					</Link>
 				</li>
 				<li className="menu-item">
-					<Link to="/React-Webshop/cart">
+					<Link to="/React-Webshop/cart" onClick={closeMenu}>
 						Cart
 						<span className="menu-cart-number">{cart.length}</span>
 					</Link>
 				</li>
 			</ul>
-			<div className="menu-hamburger">
-				<GiHamburgerMenu />
-			</div>
 		</nav>
 	);
 };
